66438cd6503291bc3c8900da7d783af02959c7ee
daring-demo-aacc <azizrocke@gmail.com>
2021-05-12 22:45:59 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var katzDeliLine =[];
+
+
+function takeANumber(katzDeli, name){
+  katzDeli.push(name)
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
+}    
+
+/*
+const takeANumber = (katzDeli, name) =>{
+  katzDeli.push(name)
+  return `Welcome, ${name}. You are number ${place.length} in line.`;
+
+}
+*/
